c7250ba477ecb23e9c70ddec53e0c3898cb6b124
codescalper <msfunbook@gmail.com>
2024-05-28 20:58:59 +0530

    feat:provider for modal

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
 'use client';
 
 import { Modal } from '@/components/ui/modal';
+import { useStoreModal } from '@/hooks/use-store-modal';
+import { useEffect } from 'react';
 
 export default function Home() {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
   return (
     <>
       <Modal
